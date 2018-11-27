8f66c404fed884bd9a01041a5cd6f798afcddbd6
jrvieira <github@jrvieira.com>
2018-11-27 02:12:06 +0000

    refactoring

--- app/Main.hs
+++ app/Main.hs
@@ -1,42 +1,22 @@
+import Color
 import System.IO
-import System.Directory
-import Codec.Picture
+import qualified Data.Map as Map
 import Data.Numbers.Primes
+import Codec.Picture
+import System.Directory
 
 -- interval
 
 ns :: [Int]
-ns = [0..7] 
+ns = [0..13] 
 
 -- png dimensions
 
 w :: Int
-w = 907 --2560
+w = 2560 --2560
 
 h :: Int
-h = 907 --1440
-
--- stdout colors
-
-data Color = Reset | R | G | Y | B | M | C | W
-
-code :: Color -> Int
-code Reset = 0
-code R = 31
-code G = 32
-code Y = 33
-code B = 34
-code M = 35
-code C = 36
-code W = 37
-
-instance Show Color where
-    show c = "\x1b[" ++ (show . code) c ++ "m"
-
-type ColorOut = Color -> String -> IO () 
-
-putClrLn :: ColorOut
-putClrLn c s = putStrLn $ show c ++ s ++ show Reset
+h = 1440 --1440
 
 -- pixels
 
